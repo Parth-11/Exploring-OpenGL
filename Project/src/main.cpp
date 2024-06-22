26c3c4c2309739829c2c90ec434dd236ffc8fdaa
Parth-11 <parth12.mgg@gmail.com>
2024-06-22 21:46:36 +0530

    Uniform variables

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader,uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.005f;
+
 
 //vertex shader
 static const std::string vertexShader = "\n\
 #version 330 \n\
 layout (location=0) in vec3 pos; \n\
 \n\
+uniform float xMove;\n\
 void main()\n\
 {\n\
-	gl_Position = vec4(0.4*pos.x,0.4*pos.y,pos.z,1.0f);\n\
+	gl_Position = vec4(0.4*pos.x+xMove,0.4*pos.y,pos.z,1.0f);\n\
 }\n\
 ";
 //Fragment Shader
@@ -83,6 +91,8 @@ void CompileShader() {
 		std::cout << "Error validating program:" << eLog << std::endl;
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 
 }
 
@@ -165,11 +175,23 @@ int main()
 		// Get + Handle user input events
 		glfwPollEvents();
 
+		if (direction) {
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+			direction = !direction;
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+		glUniform1f(uniformXMove, triOffset);
+
 		glBindVertexArray(VAO);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
