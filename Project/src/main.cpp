429a688f7abfa2c664fcbd445c68b641e675e29c
Parth-11 <parth12.mgg@gmail.com>
2024-06-24 14:31:42 +0530

    rotation

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -9,6 +9,7 @@
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
 GLuint VAO, VBO, shader,uniformModel;
 
@@ -16,6 +17,7 @@ bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
 float triIncrement = 0.005f;
+float currAngle = 0.0f;
 
 
 //vertex shader
@@ -188,6 +190,11 @@ int main()
 		if (abs(triOffset) >= triMaxOffset)
 			direction = !direction;
 
+		currAngle += 0.1f;
+		if (currAngle >= 360) {
+			currAngle -= 360;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -195,7 +202,9 @@ int main()
 		glUseProgram(shader);
 
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, currAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		
 		
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
